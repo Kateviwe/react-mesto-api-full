d489e99d7b6f9a0d5cc66e0117693334555ff10a
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-21 21:45:44 +0300

    fix: likes_9

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -9,6 +9,7 @@ const Card = require('../models/card');
 
 module.exports.getAllCards = (req, res, next) => {
   Card.find({})
+    .populate('likes')
     .then((cards) => res.send(cards))
     // catch(next) аналогична catch(err => next(err))
     .catch(next);
@@ -61,7 +62,7 @@ module.exports.putLikeToCard = (req, res, next) => {
     $addToSet: { likes: req.user._id },
     // "new: true" вернет видоизмененный массив, а не оригинал
   }, { new: true })
-    .populate(['likes'])
+    .populate('likes')
     .orFail(new NotFoundError('Запрашиваемая карточка не найдена'))
     .then((card) => res.send({ message: `Вы поставили лайк карточке с id: ${card._id}` }))
     .catch((err) => {
@@ -79,7 +80,7 @@ module.exports.deleteLikeOfCard = (req, res, next) => {
     // Если пользователь уже лайкал карточку - удалим лайк, иначе - нет
     $pull: { likes: req.user._id },
   }, { new: true })
-    .populate(['likes'])
+    .populate('likes')
     .orFail(new NotFoundError('Запрашиваемая карточка не найдена'))
     .then((card) => res.send({ message: `Вы убрали лайк с карточки с id: ${card._id}` }))
     .catch((err) => {
