19cd621399253f82c4961c1e25a09b9492f5038b
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-22 02:46:46 +0300

    fix: login_3

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -37,13 +37,14 @@ function Login({
     function handleLoginFormSubmit(e) {
         e.preventDefault();
         auth.authorize(email, password)
-        // .then((res) => {
-        //     //По факту проверяем зарегистрирован ли такой пользователь
-        //     if(res.token){
-        //         localStorage.setItem('token', res.token);
-        //         return res;
-        //     }
-        // })
+        .then((res) => {
+            //По факту проверяем зарегистрирован ли такой пользователь
+            console.log(res);
+            if(res.token){
+                localStorage.setItem('token', res.token);
+                return res;
+            }
+        })
         .then((res) => {
             if(res) {
                 onLogin();
