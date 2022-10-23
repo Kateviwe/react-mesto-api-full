b7b20f6b4b2523e7ab9f6f3bfda66ae436f7a5cc
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-23 21:58:31 +0300

    fix: login_34

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -44,9 +44,8 @@ module.exports.postNewCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((doc) => { doc.populate(['owner', 'likes']).save(); })
+    .then((doc) => { doc.populate(['owner', 'likes']).execPopulate(); })
     .then((card) => {
-      console.dir(card);
       res.send(card);
     })
     .catch((err) => {
