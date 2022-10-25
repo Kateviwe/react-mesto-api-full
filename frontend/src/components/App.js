83a39855444213b0b081c66831c95a2155b565e1
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-25 19:48:46 +0300

    fix: login_2.3

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -168,18 +168,16 @@ function App() {
     }
 
     const getUserEmail = React.useCallback(() => {
-        if (loggedIn) {
-            auth.getContentFromToken()
-            .then((res) => {
-                if(res) {
-                    setLoggedIn(true);
-                    setCurrentEmail(res.email);
-                    history.push('/');
-                }
-            })
-            .catch((err) => console.log(err));
-        }
-    }, [history, loggedIn])
+         auth.getContentFromToken()
+        .then((res) => {
+            if(res) {
+                setLoggedIn(true);
+                setCurrentEmail(res.email);
+                history.push('/');
+            }
+        })
+        .catch((err) => console.log(err));
+    }, [history])
 
     function getCookiesDelete () {
         auth.getUserExitFromProfile()
@@ -190,8 +188,10 @@ function App() {
     }
 
     React.useEffect(() => {
-        getUserEmail();
-    }, [getUserEmail]);
+        if (loggedIn) {
+            getUserEmail();
+        }
+    }, [getUserEmail, loggedIn]);
 
     function getRegisteredIn() {
         setRegisteredIn(true);
