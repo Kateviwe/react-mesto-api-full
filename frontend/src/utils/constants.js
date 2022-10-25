8764a3b7d195ccf44d80e5db5de9037f31cb1ad3
Ekaterina <67003252+Kateviwe@users.noreply.github.com>
2022-10-25 23:44:38 +0300

    fix: port_5

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'https://api.mesto.Kateviwe.nomoredomains.icu';
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5555' : 'https://api.mesto.Kateviwe.nomoredomains.icu';
 export default BASE_URL;
